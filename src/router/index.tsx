3e4f03d49a810a14b673cc7908540904e1a6d772
Võ Công Đạt <vo.cong.dat.fpt@gmail.com>
2025-07-08 22:08:19 +0700

    feat(404): add ui not found

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,6 +8,7 @@ import ProtectedLayout from "./Protected";
 const HomePage = lazy(() => import("@/pages/home"));
 const SignInPage = lazy(() => import("@/pages/sign-in"));
 const SignUpPage = lazy(() => import("@/pages/sign-up"));
+const NotFoundPage = lazy(() => import("@/pages/not-found"));
 
 export const Routers = () => {
   return (
@@ -20,6 +21,7 @@ export const Routers = () => {
         <Route element={<ProtectedLayout />}>
           <Route path={routers.HOME} element={<HomePage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
